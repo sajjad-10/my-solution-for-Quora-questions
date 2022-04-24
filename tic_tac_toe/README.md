3bb78501de91069c4bb5660d4784b56b119a4d44
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-04-24 22:15:53 +0430

    winner add

---
+++ tic_tac_toe/README.md
@@ -0,0 +1,7 @@
+
+## Tic Tac Toe
+## Quiz link
+[Quora Ques](https://quera.org/problemset/66544/).
+
+
+
--- tic_tac_toe/script.js
+++ tic_tac_toe/script.js
@@ -13,7 +13,47 @@ function handleCellPlayed(clickedCell, clickedCellIndex) {
   clickedCell.innerHTML = currentPlayer;
 }
 
+function empty(str){
+  if(str==="") return true;
+  return false;
+}
+
 function handlePlayerChange() {
+  var winner = "";
+  if(!empty(gameState[0]) && gameState[0]==gameState[1] && gameState[1]==gameState[2]){
+    winner=gameState[0];
+  }else if(!empty(gameState[3]) && gameState[3]==gameState[4]&&gameState[4]==gameState[5]){
+    winner=gameState[3];
+  }
+  else if(!empty(gameState[6]) && gameState[6]==gameState[7]&&gameState[7]==gameState[8]){
+    winner=gameState[6];
+  }
+  else if(!empty(gameState[0]) && gameState[0]==gameState[3]&&gameState[3]==gameState[6]){
+    winner=gameState[0];
+  }
+  else if(!empty(gameState[1]) && gameState[1]==gameState[4]&&gameState[4]==gameState[7]){
+    winner=gameState[1];
+  }
+  else if(!empty(gameState[2]) && gameState[2]==gameState[5]&&gameState[5]==gameState[8]){
+    winner=gameState[2];
+  }
+  else if(!empty(gameState[0]) && gameState[0]==gameState[4]&&gameState[4]==gameState[8]){
+    winner=gameState[0];
+  }
+  else if(!empty(gameState[2]) && gameState[2]==gameState[4]&&gameState[4]==gameState[6]){
+    winner=gameState[2];
+  }
+  if(!empty(winner)){
+    gameActive=false;
+    statusDisplay.innerHTML = "Player "+winner+" has won";
+    return;
+  }
+  var fulltable=gameState.includes('');
+  if(!fulltable){
+    gameActive=false;
+    statusDisplay.innerHTML = "Game ended in a draw";
+    return;
+  }
   currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
   statusDisplay.innerHTML = currentPlayerTurn();
 }
@@ -24,10 +64,10 @@ function handleCellClick(clickedCellEvent) {
     clickedCell.getAttribute('data-cell-index')
   );
 
-  if (gameState[clickedCellIndex] !== '') {
+  if (gameState[clickedCellIndex] !== '' || gameActive==false) {
     return;
   }
-
+  
   handleCellPlayed(clickedCell, clickedCellIndex);
   handlePlayerChange();
 }
@@ -37,6 +77,7 @@ function handleRestartGame() {
   gameState = ['', '', '', '', '', '', '', '', ''];
   statusDisplay.innerHTML = currentPlayerTurn();
   document.querySelectorAll('.cell').forEach(cell => (cell.innerHTML = ''));
+  gameActive=true;
 }
 
 document
