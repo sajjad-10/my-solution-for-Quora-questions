0564979395cfb93ab663f844f7058b7975184a75
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-04-26 22:29:15 +0430

    done quiz

---
+++ background-color-switch/app.js
@@ -0,0 +1,10 @@
+var itemsEl = document.querySelectorAll(".button");
+var bodyEl = document.body;
+// document.body.style.backgroundColor = 'blue'
+itemsEl.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        const colorSelected = e.target.getAttribute("id");
+        console.log(colorSelected);
+        bodyEl.style.backgroundColor = colorSelected
+    });
+});
