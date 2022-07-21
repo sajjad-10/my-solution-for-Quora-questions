ace0bb1ee03dbb9cfc26e9bdf9f4471aaea6cb45
sajjad-10 <sajjadfallahi.official@gmail.com>
2022-07-21 20:38:51 +0430

    try0

--- card-memory-game/src/components/Game.js
+++ card-memory-game/src/components/Game.js
@@ -1,9 +1,18 @@
+import { useState } from 'react'
 import Card from "./Card";
-
+import { cardsData } from '../cards'
 function Game() {
+  const [cards, setCards] = useState(cardsData)
+  const cardClicked = (card,id) => {
+    console.log('Card Select',card)
+    console.log('Id Select',id)
+    setCards()
+  }
+  console.log(cardsData);
   return (
     <section className="memory-game">
-      {/* <Card card={card} onClick={} /> */}
+      {cards.map((card) => <Card card={card} key={card.id} onClick={() => cardClicked(card,card.id)} />)}
+
       <h1>SSS</h1>
     </section>
   );
